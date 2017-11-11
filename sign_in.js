9c16ba9d8244bcf05e390d471a818ce78e16bf8f
zkulphong <zkulphong@gmail.com>
2017-11-11 08:07:18 -0600

    started sign-in backend

--- sign_in.js
+++ sign_in.js
@@ -1,1 +1,18 @@
 
+class Sign_In()
+{
+  constructor()
+  {
+    this.username = document.getElementByID('username').value;
+    this.password = document.getElementByID('password').value;
+  }
+
+  check
+
+
+
+
+
+
+
+}
